06b9543d4dc3e587e2ad62fb9be2c7f063b700e2
cis-WIn <kr.chis@gmail.com>
2019-12-02 15:05:03 +0900

    css apply

---
+++ src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <br/><br/><br/><br/>
+        admin
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+
+    },
+
+    data(){
+        return{
+            
+        }
+    },
+
+    methods:{
+
+    }
+    
+}
+</script>

7a75293460e2829a2c3eed1d2ade8e849c16ed0c
InseokChoi <kr.chis@gmail.com>
2019-12-17 23:33:59 +0900

    test

--- src/components/Monitering.vue
+++ src/components/Monitering.vue
@@ -5,10 +5,10 @@
         <div class="view overlay"></div>
 
         <!--Card content-->
-        <div class="card-body">
+        <div class="card-body" >
             <!--Title-->
             <h4 class="card-title">모니터링</h4>
-            <line-chart :chart-data="datacollection"></line-chart>
+            <line-chart :chart-data="datacollection" ></line-chart>
             <button @click="fillData()">Randomize</button>
         </div>
     </div>
@@ -24,8 +24,12 @@ export default {
     },
     data() {
         return {
-            datacollection: null
+            datacollection: null,
+           
         };
+    },
+    computed: {
+       
     },
     mounted() {
         this.fillData();
@@ -33,17 +37,65 @@ export default {
     methods: {
         fillData() {
             this.datacollection = {
-                labels: [this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR()],
+                labels: [
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR(),
+                    this.getR()
+                ],
                 datasets: [
                     {
                         label: "온도",
                         backgroundColor: "#f87979",
-                        data: [this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR()]
+                        data: [
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR()
+                        ]
                     },
                     {
                         label: "배터리",
                         backgroundColor: "#4DDF5F",
-                        data: [this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR(), this.getR()]
+                        data: [
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR(),
+                            this.getR()
+                        ]
                     }
                 ]
             };
